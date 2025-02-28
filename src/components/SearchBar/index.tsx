e9db5bb663460c4d6c74798d30183dc2538f4eb3
Thiago Leite <leite.tjs@gmail.com>
2025-02-28 14:03:59 -0300

    feat: debounce

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,12 +1,29 @@
+import { useState, useEffect, useContext } from 'react';
 import { Input } from '../Input';
 import styles from './styles.module.css';
+import { EmployeeContext } from '../../context/employeeProvider';
 
 export function SearchBar() {
+  const { handleSearchTerm, searchTerm } = useContext(EmployeeContext);
+  const [inputValue, setInputValue] = useState(searchTerm);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      handleSearchTerm(inputValue);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [inputValue, handleSearchTerm]);
+
   return (
     <section className={styles.searchContainer}>
       <h1>Funcionários</h1>
       <form className={styles.formContainer}>
-        <Input placeholder="Pesquisar" />
+        <Input
+          placeholder="Pesquisar"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
       </form>
     </section>
   );
